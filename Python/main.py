6434d8f5d91a93a210486a397a05ee974d2b71fe
Pranshu-S <pranshushukla.191me260@nitk.edu.in>
2021-03-20 15:45:34 +0530

    Created Adj Matrix Function

--- Python/main.py
+++ Python/main.py
@@ -2,9 +2,20 @@ import env,plotting
 import numpy as np
 import math
 
+def get_matrix(obstacles_set):
+    obstacles = list(obstacles_set)
+    matrix = np.zeros((100,100))
+    
+    for obs in obstacles:
+        matrix[int(obs[0])][int(obs[1])] = 1
+    
+    return matrix
+
 def main():
     plot = plotting.Plotting()
     plot.plot_grid("Map")
+
+    adj_matrix = get_matrix(plot.ret_map())
 
 if __name__ == '__main__':
     main()
